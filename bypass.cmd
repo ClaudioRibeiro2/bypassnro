1417037b26f7ada779ad90dcf1154ffdaf7f028f
ClaudioRibeiro2 <61056002+ClaudioRibeiro2@users.noreply.github.com>
2025-11-20 19:15:19 +0000

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\cconti\unattend.xml https://raw.githubusercontent.com/ClaudioRibeiro2/bypassnro/unattend.xml
-%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\cconti\unattend.xml /reboot
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ClaudioRibeiro2/bypassnro/refs/heads/main/unattend.xml
+%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
